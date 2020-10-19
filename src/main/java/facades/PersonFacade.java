82b1ed543584af2ffc3687a863a383d3dde06f14
Pelle Rasmussen <cph-pr128@cphbusiness.com>
2020-10-19 12:36:28 +0200

    addPerson added to facade

--- src/main/java/facades/PersonFacade.java
+++ src/main/java/facades/PersonFacade.java
@@ -1,12 +1,11 @@
 package facades;
 
 import dto.PersonDTO;
-import entities.Person;
+import entities.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class PersonFacade implements IPersonFacade{
@@ -55,5 +54,75 @@ public class PersonFacade implements IPersonFacade{
 
 
         return dtoList;
+    }
+
+    @Override
+    public PersonDTO addPerson(PersonDTO personDTO) {
+
+        EntityManager em = getEntityManager();
+
+        try {
+            Person newPerson = preparePerson(personDTO);
+            em.getTransaction().begin();
+            em.persist(newPerson);
+            em.getTransaction().commit();
+
+            return new PersonDTO(newPerson);
+
+        } finally {
+            em.close();
+        }
+    }
+
+//     @Override
+//    public PersonDTO getPersonByPhone(String phone) {
+//
+//        EntityManager em = getEntityManager();
+//
+//        Query q1 = em.createQuery("SELECT p.id from Phone p where p.number = :phone");
+//        q1.setParameter("phone", phone);
+//        int phoneID = (int) q1.getSingleResult();
+//
+//        Query q2 = em.createQuery()
+//
+//    }
+
+    private Person preparePerson (PersonDTO personDTO){
+
+        Person newPerson = new Person();
+        CityInfo newCityInfo = new CityInfo(personDTO.getZipCode(), personDTO.getCity());
+        Address newAddress = new Address(personDTO.getStreet(), newCityInfo);
+        newPerson.setFirstName(personDTO.getFirstName());
+        newPerson.setLastName(personDTO.getLastName());
+        newPerson.setAddress(newAddress);
+        newPerson.setPhoneNumbers(personDTO.getPhoneNumbers());
+        newPerson.setEmail(personDTO.getEmail());
+        newPerson.setDateCreated(new Date());
+        newPerson.setHobbies(personDTO.getHobbies());
+
+        return newPerson;
+    }
+
+    public static void main(String[] args) {
+
+        Person person = new Person("Pelle@mail.dk","Pelle","Rasmussen");
+        Address address = new Address("Smedeløkken 66", new CityInfo(3770,"Allinge"));
+        List<Hobby> hobbies = new ArrayList<>();
+        hobbies.add(new Hobby("Tennis","ko lort", "Prutfis", "lort"));
+        List<Phone> phones = new ArrayList<>();
+        phones.add(new Phone("2010210102", "work"));
+        phones.add(new Phone("201102", "home"));
+
+        person.setPhoneNumbers(phones);
+        person.setHobbies(hobbies);
+        person.setAddress(address);
+
+        PersonDTO personDTO = new PersonDTO(person);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+
+        PersonFacade.getPersonFacade(emf).addPerson(personDTO);
+
+
     }
 }
