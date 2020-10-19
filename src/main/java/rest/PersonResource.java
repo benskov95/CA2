16160683f12e2989b2821d870b5e2df1d4c06d53
Matt Thomsen <cph-mt312@cphbusiness.dk>
2020-10-19 11:33:24 +0200

    added addPerson endpoint

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest;
 
-/**
- *
- * @author mattg
- */
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.PersonDTO;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import utils.EMF_Creator;
+
+@Path("persons")
 public class PersonResource {
+    
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    private static final PersonFacade FACADE = PersonFacade.getPersonFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getAllPersons() {
+        List<PersonDTO> personDTOs = FACADE.getAllPersons();
+        return new Gson().toJson(personDTOs);
+    }
+    
+    @POST
+    @Produces({MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_JSON})
+    public String addPerson(String person) {
+        PersonDTO personDTO = GSON.fromJson(person, PersonDTO.class);
+        personDTO = FACADE.addPerson(personDTO);
+        return new Gson().toJson(personDTO);
+    }
+    
     
 }
