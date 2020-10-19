5e95d0da61a799856616d9b24cf777a606839276
Benjamin S <ben.sommer.skovgaard@gmail.com>
2020-10-19 12:36:22 +0200

    updated hobby constructor again

--- src/main/java/entities/Hobby.java
+++ src/main/java/entities/Hobby.java
@@ -34,6 +34,9 @@ public class Hobby implements Serializable {
 
     public Hobby(String name, String wikiLink, String category, String type) {
         this.name = name;
+        this.wikiLink = wikiLink;
+        this.category = category;
+        this.type = type;
     }
 
     public Hobby(String name) {
