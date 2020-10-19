5977c3856070fe7342565ef76771ca8408665f5f
Pelle Rasmussen <cph-pr128@cphbusiness.com>
2020-10-19 13:18:51 +0200

    minor fix

--- src/main/java/rest/CityInfoResource.java
+++ src/main/java/rest/CityInfoResource.java
@@ -1,27 +1,27 @@
-package rest;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import entities.CityInfo;
-import java.util.List;
-import javax.persistence.EntityManagerFactory;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import utils.EMF_Creator;
-
-@Path("zipcodes")
-public class CityInfoResource {
-
-    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-    private static final CityInfoFacade FACADE = CityInfoFacade.getCityInfoFacade(EMF);
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-
-    @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    public String getAllZipCodes() {
-        List<CityInfo> citiesInfo = FACADE.getAllZipCodes();
-        return new Gson().toJson(citiesInfo);
-    }
-}
+//package rest;
+//
+//import com.google.gson.Gson;
+//import com.google.gson.GsonBuilder;
+//import entities.CityInfo;
+//import java.util.List;
+//import javax.persistence.EntityManagerFactory;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.MediaType;
+//import utils.EMF_Creator;
+//
+//@Path("zipcodes")
+//public class CityInfoResource {
+//
+//    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+//    private static final CityInfoFacade FACADE = CityInfoFacade.getCityInfoFacade(EMF);
+//    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+//
+//    @GET
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public String getAllZipCodes() {
+//        List<CityInfo> citiesInfo = FACADE.getAllZipCodes();
+//        return new Gson().toJson(citiesInfo);
+//    }
+//}
