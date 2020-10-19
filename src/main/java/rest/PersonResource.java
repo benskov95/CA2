1197ccc2a8dad0d260d482e1d5551af1a0971830
Matt Thomsen <cph-mt312@cphbusiness.dk>
2020-10-19 11:41:56 +0200

    added editPerson endpoint

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -8,7 +8,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import utils.EMF_Creator;
@@ -36,5 +38,13 @@ public class PersonResource {
         return new Gson().toJson(personDTO);
     }
     
-    
+    @PUT
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String editPerson(@PathParam("id") int id, String person) {
+        PersonDTO personDTO = GSON.fromJson(person, PersonDTO.class);
+        personDTO.setId(id);
+        personDTO = FACADE.editPerson(personDTO);
+        return new Gson().toJson(personDTO);
+    }
 }
